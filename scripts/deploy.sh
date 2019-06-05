09f92cf03367f514244ff0d3f43bc46a9fa93d2c
Tony Beninate <tonybeninate@icloud.com>
2019-06-05 18:16:40 -0400

    Deploy script update.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,6 +2,6 @@
 
 echo -e "Deploying...\n"
 
-scp -r ~/Documents/Apps/racing-strats/* tony@134.209.70.195:/var/www/racingstrats.com/html
+scp -r ~/Documents/Apps/racing-strats/* tony@68.183.17.207:/var/www/racingstrats.com/html
 
 echo -e "\nDeploy complete."
